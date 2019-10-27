1323db7a41e0aa89312066eca62afbe2496f09d9
Ian Be <ian84be@gmail.com>
2019-10-27 15:09:47 -0500

    scaffold react app with material ui

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, CssBaseline } from '@material-ui/core';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </>
+  );
+}
+
+export default App;
