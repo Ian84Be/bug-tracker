3993b25b87ef4588b0dcd05c2057cc3c6ccba470
Ian Be <ian84be@gmail.com>
2019-10-27 15:40:21 -0500

    navbar template

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import { Button, CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <Navbar />
       <Button variant="contained" color="primary">
         Hello World
       </Button>
