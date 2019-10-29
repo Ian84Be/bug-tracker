732c21b46d46cc72dec5084de2d8ccb1738eb5eb
Ian Be <ian84be@gmail.com>
2019-10-29 14:16:05 -0500

    add projects, teams, team_list, project_team_list, assignments, ticket_labels, actions & update data schema

--- server/data/migrations/20191027225245_init.js
+++ server/data/migrations/20191027225245_init.js
@@ -8,26 +8,126 @@ exports.up = function(knex) {
 					.unique();
 			tbl.string('password').notNullable();
 			tbl.string('email');
+			tbl.string('role');
+	})
+	.createTable('projects', tbl => {
+		tbl.increments();
+		tbl.string('name').notNullable().unique();
+	})
+	.createTable('teams', tbl => {
+		tbl.increments();
+		tbl.string('name').notNullable().unique();
+	})
+	.createTable('team_list', tbl => {
+		tbl.increments();
+		tbl.integer('user_id')
+				.references('id')
+				.inTable('users')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE')
+				.notNullable();
+		tbl.integer('team_id')
+				.references('id')
+				.inTable('teams')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE')
+				.notNullable();
+		tbl.string('role');
+	})
+	.createTable('project_team_list', tbl => {
+		tbl.increments();
+		tbl.integer('project_id')
+				.references('id')
+				.inTable('users')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE')
+				.notNullable();
+		tbl.integer('team_id')
+				.references('id')
+				.inTable('teams')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE')
+				.notNullable();
+		tbl.string('role');
 	})
 	.createTable('tickets', tbl => {
 			tbl.increments();
-			tbl.string('date').notNullable();
-			tbl.string('from')
+			tbl.string('date_created').notNullable();
+			tbl.string('date_updated').notNullable();
+			tbl.integer('from_user_id')
 				.references('id')
 				.inTable('users')
+				.onDelete('RESTRICT')
+				.onUpdate('CASCADE')
 				.notNullable();
-			tbl.string('project');
+			tbl.string('project_id')
+				.references('id')
+				.inTable('projects')
+				.onDelete('RESTRICT')
+				.onUpdate('CASCADE')
+				.notNullable();
 			tbl.string('subject').notNullable();
 			tbl.text('content').notNullable();
-			tbl.text('status')
+			tbl.string('status')
 				.notNullable()
 				.defaultTo('new');
-			tbl.text('priority');
+			tbl.string('priority');
 	})
+	.createTable('ticket_labels', tbl => {
+		tbl.increments();
+		tbl.integer('content').notNullable();
+		tbl.integer('ticket_id')
+			.references('id')
+			.inTable('tickets')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE')
+			.notNullable();
+	})
+	.createTable('assignments', tbl => {
+		tbl.increments();
+		tbl.integer('user_id')
+			.references('id')
+			.inTable('users')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE')
+			.notNullable();
+		tbl.integer('ticket_id')
+			.references('id')
+			.inTable('tickets')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE')
+			.notNullable();
+	})
+	.createTable('actions', tbl => {
+		tbl.increments();
+		tbl.integer('ticket_id')
+			.references('id')
+			.inTable('tickets')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE')
+			.notNullable();
+		tbl.integer('user_id')
+			.references('id')
+			.inTable('users')
+			.onDelete('RESTRICT')
+			.onUpdate('CASCADE')
+			.notNullable();
+		tbl.string('date').notNullable();
+		tbl.text('content').notNullable();
+	})
+	
+	
 };
 
 exports.down = function(knex) {
   return knex.schema
+        .dropTableIfExists('actions')
+        .dropTableIfExists('assignments')
+        .dropTableIfExists('ticket_labels')
         .dropTableIfExists('tickets')
+        .dropTableIfExists('project_team_list')
+        .dropTableIfExists('team_list')
+        .dropTableIfExists('teams')
+        .dropTableIfExists('projects')
         .dropTableIfExists('users');
 };
